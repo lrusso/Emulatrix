6f631878d0ad92685931e18eb8462a8a47b667c2
Leonardo Russo <info@lrusso.com>
2020-08-25 15:06:15 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,2 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/',
-];
-
 self.addEventListener('fetch', function(event) {})
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+self.addEventListener('install', function(event){});
